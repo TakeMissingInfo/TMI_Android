b7ef158f01da9cca7a015fce3dc3abecfb0c6c23
garam918 <62510764+garam918@users.noreply.github.com>
2021-01-26 17:18:09 +0900

    Feat : 선택한 지역과 대분류를 상세 분류 Activity로 넘김

--- app/src/main/java/com/example/garam/takemissinginfo/MainCategory.kt
+++ app/src/main/java/com/example/garam/takemissinginfo/MainCategory.kt
@@ -1,5 +1,6 @@
 package com.example.garam.takemissinginfo
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -13,6 +14,8 @@ class MainCategory : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_category)
 
+        var location = ""
+
         val items = resources.getStringArray(R.array.city)
         val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,items)
 
@@ -21,6 +24,19 @@ class MainCategory : AppCompatActivity() {
         citySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener,
             AdapterView.OnItemClickListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                when(p2){
+                    0 -> location = "서울특별시"
+                    1 -> location = "경기도"
+                    2 -> location = "부산광역시"
+                    3 -> location = "대전광역시"
+                    4 -> location = "대구광역시"
+                    5 -> location = "광주광역시"
+                    6 -> location = "경상남도"
+                    7 -> location = "경상북도"
+                    8 -> location = "강원도"
+                    9 -> location = "세종특별자치시"
+                }
+                imageButtonClickListener(location)
 
             }
 
@@ -32,5 +48,33 @@ class MainCategory : AppCompatActivity() {
 
             }
         }
+
+
+    }
+
+    private fun imageButtonClickListener(location: String){
+        disableMenu.setOnClickListener {
+            nextIntent("장애인",location)
+        }
+
+        oldManMenu.setOnClickListener {
+            nextIntent("노인",location)
+        }
+
+        lowIncomeMenu.setOnClickListener {
+            nextIntent("저소득층",location)
+        }
+
+        childHeadMenu.setOnClickListener {
+            nextIntent("소년소녀 가장",location)
+        }
+
+    }
+
+    private fun nextIntent(menuName: String,location: String){
+        val nextIntent = Intent(this,SubClass::class.java)
+        nextIntent.putExtra("menuName",menuName)
+        nextIntent.putExtra("location",location)
+        startActivity(nextIntent)
     }
 }
--- app/src/main/java/com/example/garam/takemissinginfo/SubClass.kt
+++ app/src/main/java/com/example/garam/takemissinginfo/SubClass.kt
@@ -2,10 +2,15 @@ package com.example.garam.takemissinginfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class SubClass : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub_class)
+        val intent = intent
+        val menuName = intent.getStringExtra("menuName")
+        val location = intent.getStringExtra("location")
+        Toast.makeText(this,"$menuName , $location", Toast.LENGTH_SHORT).show()
     }
 }
